9d0817f73deb2469b9c89ed77ace1df2036fb5f2
OliverSchlueter <OliverSchlueter@users.noreply.github.com>
2024-08-02 22:46:58 +0200

    Add tests for 1.21

---
+++ implementations/1_21/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java-library")
+    id("io.papermc.paperweight.userdev") version "1.7.1"
+}
+
+val minecraftVersion = "1.21"
+
+paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
+
+dependencies {
+    paperweight.paperDevBundle("$minecraftVersion-R0.1-SNAPSHOT")
+    compileOnly(project(":api"))
+
+    testImplementation(project(":api"))
+    testImplementation(project(":implementations:1_20_6"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,5 +4,6 @@ include(":api")
 include(":factories")
 include(":implementations")
 include(":implementations:1_20_6")
+include(":implementations:1_21")
 
 include(":test_plugin")
