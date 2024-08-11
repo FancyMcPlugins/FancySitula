bfea20928cab957a5a1a10fc5efee888897d7acc
OliverSchlueter <OliverSchlueter@users.noreply.github.com>
2024-08-11 17:30:28 +0200

    Add tests for 1.21.1

---
+++ implementations/1_21_1/src/test/java/packets/ClientboundRotateHeadPacketImplTest.java
@@ -0,0 +1,22 @@
+package packets;
+
+import de.oliver.fancysitula.api.utils.AngelConverter;
+import de.oliver.fancysitula.api.utils.reflections.ReflectionUtils;
+import de.oliver.fancysitula.versions.v1_20_6.packets.ClientboundRotateHeadPacketImpl;
+import net.minecraft.network.protocol.game.ClientboundRotateHeadPacket;
+import org.junit.jupiter.api.Test;
+
+class ClientboundRotateHeadPacketImplTest {
+
+    @Test
+    void createPacket() throws Exception {
+        int entityId = 184;
+        float headYaw = 45;
+
+        ClientboundRotateHeadPacketImpl packet = new ClientboundRotateHeadPacketImpl(entityId, (byte) headYaw);
+        ClientboundRotateHeadPacket createdPacket = (ClientboundRotateHeadPacket) packet.createPacket();
+
+        assert ReflectionUtils.getField(createdPacket, "entityId").equals(entityId);
+        assert createdPacket.getYHeadRot() == AngelConverter.degreesToVanillaByte(headYaw);
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,5 +5,6 @@ include(":factories")
 include(":implementations")
 include(":implementations:1_20_6")
 include(":implementations:1_21")
+include(":implementations:1_21_1")
 
 include(":test_plugin")
