f48b2437625940a45d010a1d6eb57d387d116041
OliverSchlueter <OliverSchlueter@users.noreply.github.com>
2024-09-15 20:23:25 +0200

    Update version to 0.0.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 allprojects {
     group = "de.oliver"
-    version = "0.0.8"
+    version = "0.0.9"
     description = "Simple, lightweight and fast library for minecraft internals"
 
     repositories {
